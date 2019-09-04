723da077f4a4be16f2413629f4a88367d2d86fd4
dxshim90 <dxshim90@gmail.com>
2019-09-04 15:31:26 +0100

    deploy

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ const cors = require("cors");
 
 const app = express();
 
-app.all("*", function(req, res, next) {
-  var origin = req.get("origin");
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.use(cors());
 
 mongoose.connect(process.env.mongoDB, { useNewUrlParser: true }, () => {
